aa6cb94d7adf2668c4965c6addcc4f3b36c9b8f2
opesnoir <goodsurroundsme@outlook.com>
2023-05-25 14:13:46 +0200

    mappenstructuur aangemaakt

---
+++ src/test/java/com/example/jubileebackendeindopdracht/JubileeBackendEindopdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jubileebackendeindopdracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JubileeBackendEindopdrachtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
