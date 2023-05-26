beb99c55c272cbda190642defe393d1cfde9bb3a
opesnoir <goodsurroundsme@outlook.com>
2023-05-26 13:42:48 +0200

    probleem pgadmin koppeling opgelost met de hulp van Paul, door port-nummer te wijzigen en spring.jpa.database-platform=org.hibernate.dialect.PostgreSQLDialect toe te voegen. Daarnaast had ik gisteren pgadmin verwijderd en weer geinstalleerd.

---
+++ src/main/java/com/example/jubileebackendeindopdracht/model/Income.java
@@ -0,0 +1,33 @@
+package com.example.jubileebackendeindopdracht.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Setter
+@Getter
+
+@Entity
+@Table (name = "incomes")
+public class Income {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    public int money;
+    public String title;
+
+
+
+
+
+}
