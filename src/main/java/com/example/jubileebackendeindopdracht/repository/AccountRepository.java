7063479a631f7e8fe934078f218f3385e4cf9a0c
opesnoir <goodsurroundsme@outlook.com>
2023-06-02 14:20:57 +0200

    transaction repository leeggehaald, account repository aangevuld met find all income en find all expense

--- src/main/java/com/example/jubileebackendeindopdracht/repository/AccountRepository.java
+++ src/main/java/com/example/jubileebackendeindopdracht/repository/AccountRepository.java
@@ -3,6 +3,13 @@ package com.example.jubileebackendeindopdracht.repository;
 import com.example.jubileebackendeindopdracht.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    //query('s)
+    List<Account> findAllByTotalIncome(BigDecimal totalIncome);
+    List<Account> findAllByTotalExpense(BigDecimal totalExpense);
 
 }
--- src/main/java/com/example/jubileebackendeindopdracht/repository/TransactionRepository.java
+++ src/main/java/com/example/jubileebackendeindopdracht/repository/TransactionRepository.java
@@ -4,27 +4,10 @@ import com.example.jubileebackendeindopdracht.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository <Transaction, Long>{
 
-    //query's
-/*    List<Transaction> findAllByDate(LocalDate date);
-    List<Transaction> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
-    List<Transaction> findAllByDateIsAfter(LocalDate date);
-    List<Transaction> findAllByDateIsBefore(LocalDate date);
-
-    List<Transaction> findAllByAmount(BigDecimal amount);*/
-    List<Transaction> findAllByCategoryIgnoreCase(String category);
-/*
-    List<Transaction> findAllByPayeeIgnoreCase(String payee);
-    List<Transaction> findAllByPaymentMethod(String paymentMethode);
-
-    List<Transaction> findAllByIncome(String income);
-    List<Transaction> findAllByExpense(String expense);
-*/
+    //query('s)
 
 }
