081a2931d740728906a3c230bb7c07f27aaa7c5a
opesnoir <goodsurroundsme@outlook.com>
2023-06-02 14:04:29 +0200

    de validatie voor de totalIncome en totalExpense verwijderd, die worden namelijk berekent door de income transacties of de expense transacties op te tellen, dat heeft dus niks met de gebruiker te maken.

--- src/main/java/com/example/jubileebackendeindopdracht/dto/AccountDto.java
+++ src/main/java/com/example/jubileebackendeindopdracht/dto/AccountDto.java
@@ -3,7 +3,6 @@ package com.example.jubileebackendeindopdracht.dto;
 import com.example.jubileebackendeindopdracht.model.Transaction;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,19 +23,12 @@ public class AccountDto {
 
     //variables declaration
     private Long id;
-
     private BigDecimal balance;
-
-    // value must be more than zero, .
-    @DecimalMin(value = "0.0", message = "Total income must be greater than zero")
     private BigDecimal totalIncome;
-
-    @DecimalMin(value = "0.0", message = "Total expense must be greater than zero")
     private BigDecimal totalExpense;
 
     // apply validation rules from transaction object
     @Valid
     private List<Transaction> transactionList;
-
 
 }
