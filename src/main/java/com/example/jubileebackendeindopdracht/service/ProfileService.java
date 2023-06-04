74c1fd193e3515fb2568b2370c65066e47de0a86
opesnoir <goodsurroundsme@outlook.com>
2023-06-04 20:01:00 +0200

    profile service van repository koppeling en constructor voorzien

--- src/main/java/com/example/jubileebackendeindopdracht/service/ProfileService.java
+++ src/main/java/com/example/jubileebackendeindopdracht/service/ProfileService.java
@@ -1,9 +1,16 @@
 package com.example.jubileebackendeindopdracht.service;
 
+import com.example.jubileebackendeindopdracht.repository.ProfileRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProfileService {
 
+    // repository for accessing profile data
+    private final ProfileRepository profileRepository;
 
+    // constructor for initializing ProfileService with the corresponding repository
+    public ProfileService(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
 }
