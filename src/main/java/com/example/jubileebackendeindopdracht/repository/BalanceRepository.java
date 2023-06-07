ebab3b761e85216d1766c3d79416dcda3909f5c6
opesnoir <goodsurroundsme@outlook.com>
2023-06-07 10:14:45 +0200

    balance repository aangemaakt, hij extends jpa

---
+++ src/main/java/com/example/jubileebackendeindopdracht/repository/BalanceRepository.java
@@ -0,0 +1,8 @@
+package com.example.jubileebackendeindopdracht.repository;
+
+import com.example.jubileebackendeindopdracht.model.Balance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BalanceRepository extends JpaRepository <Balance, Long> {
+    // query('s)
+}
