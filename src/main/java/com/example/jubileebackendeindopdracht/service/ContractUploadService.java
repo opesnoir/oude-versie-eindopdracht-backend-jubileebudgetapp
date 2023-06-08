78e2b0ddf0ef2af36f0be3886a3d9a7611f19c4d
opesnoir <goodsurroundsme@outlook.com>
2023-06-08 11:38:53 +0200

    contract upload service van de helper methode van en naar dto opgeschreven

--- src/main/java/com/example/jubileebackendeindopdracht/service/ContractUploadService.java
+++ src/main/java/com/example/jubileebackendeindopdracht/service/ContractUploadService.java
@@ -1,5 +1,7 @@
 package com.example.jubileebackendeindopdracht.service;
 
+import com.example.jubileebackendeindopdracht.dto.ContractUploadDto;
+import com.example.jubileebackendeindopdracht.model.ContractUpload;
 import com.example.jubileebackendeindopdracht.repository.ContractUploadRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +18,25 @@ public class ContractUploadService {
     //download file
     //delete file
 
+    //helper methodes
+    public ContractUploadDto transferContractUploadToContractUploadDto(ContractUpload contractUpload){
+
+        ContractUploadDto contractUploadDto = new ContractUploadDto();
+        contractUploadDto.setPayee(contractUpload.getPayee());
+        contractUploadDto.setContractPurpose(contractUpload.getContractPurpose());
+        contractUploadDto.setContractPdfUrl(contractUpload.getContractPdfUrl());
+
+        return contractUploadDto;
+    }
+
+    public ContractUpload transferContractUploadDtoToContractUpload(ContractUploadDto contractUploadDto){
+
+        ContractUpload contractUpload = new ContractUpload();
+        contractUpload.setPayee(contractUploadDto.getPayee());
+        contractUpload.setContractPurpose(contractUploadDto.getContractPurpose());
+        contractUpload.setContractPdfUrl(contractUploadDto.getContractPdfUrl());
+
+        return contractUpload;
+    }
 
 }
