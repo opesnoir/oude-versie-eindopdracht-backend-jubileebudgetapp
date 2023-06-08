8978986a9a38b77dda99bd155cb0ac299e26ef83
opesnoir <goodsurroundsme@outlook.com>
2023-06-08 11:29:55 +0200

    controller upload controller gemaakt, variabel en constructor toegevoegd. daarnaast aangegeven wat ik hier wil gaan doen

--- src/main/java/com/example/jubileebackendeindopdracht/controller/ContractUploadController.java
+++ src/main/java/com/example/jubileebackendeindopdracht/controller/ContractUploadController.java
@@ -1,4 +1,21 @@
 package com.example.jubileebackendeindopdracht.controller;
 
+import com.example.jubileebackendeindopdracht.service.ContractUploadService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contractuploads")
 public class ContractUploadController {
+
+    private final ContractUploadService contractUploadService;
+
+    public ContractUploadController(ContractUploadService contractUploadService) {
+        this.contractUploadService = contractUploadService;
+    }
+
+    //post
+    //get
+    //delete
+
 }
