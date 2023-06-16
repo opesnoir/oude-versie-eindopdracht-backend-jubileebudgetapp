8d881f7be5abc55e16807f8574de47028991f647
opesnoir <goodsurroundsme@outlook.com>
2023-06-16 17:58:57 +0200

    user dto aangemaakt op basis van de user model, code runt nog goed

--- src/main/java/com/example/jubileebackendeindopdracht/dtos/UserDto.java
+++ src/main/java/com/example/jubileebackendeindopdracht/dtos/UserDto.java
@@ -1,5 +1,6 @@
 package com.example.jubileebackendeindopdracht.dtos;
 
+import com.example.jubileebackendeindopdracht.models.Authority;
 import com.example.jubileebackendeindopdracht.models.Upload;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
@@ -7,6 +8,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,15 +19,22 @@ import lombok.Setter;
 
 public class UserDto {
 
-    private Long id;
     @NotBlank
     @Size(min = 3, max = 50, message = " at least 3 to 50 characters long")
     private String username;
+
     @Size(min = 8, max = 50)
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$", message = "The password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")
     private String password;
+
+    public Boolean enabled;
+    public String apikey;
+
     @Email
     private String email;
+
+    public Set<Authority> authorities;
+
     private Long accountId;
 
     @Valid
