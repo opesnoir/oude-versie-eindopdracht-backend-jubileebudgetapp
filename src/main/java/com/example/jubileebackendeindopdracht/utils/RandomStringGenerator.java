b353f3f92ce02c3bdf2c2a4d2119127218dfe8ce
opesnoir <goodsurroundsme@outlook.com>
2023-06-18 07:39:16 +0200

    randomstringgenerator gemaakt en van code voorzien

--- src/main/java/com/example/jubileebackendeindopdracht/utils/RandomStringGenerator.java
+++ src/main/java/com/example/jubileebackendeindopdracht/utils/RandomStringGenerator.java
@@ -1,4 +1,19 @@
 package com.example.jubileebackendeindopdracht.utils;
+import java.util.Random;
 
 public class RandomStringGenerator {
+    public static String generateAlphaNumeric(int length) {
+        int leftLimit = 48; // ASCII value for '0'
+        int rightLimit = 122; // ASCII value for 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        return generatedString;
+    }
 }
