b8f6a8fac5bab4715a562f818bcfac2104b8a049
opesnoir <goodsurroundsme@outlook.com>
2023-06-19 15:29:21 +0200

    payloadmap (gegevensuitwisseling) AuthenticationResponse en AuthenticationRequest gemaakt

--- src/main/java/com/example/jubileebackendeindopdracht/payload/AuthenticationRequest.java
+++ src/main/java/com/example/jubileebackendeindopdracht/payload/AuthenticationRequest.java
@@ -1,4 +1,19 @@
 package com.example.jubileebackendeindopdracht.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Getter
+@Setter
+
 public class AuthenticationRequest {
+
+    private String username;
+    private String password;
+
 }
--- src/main/java/com/example/jubileebackendeindopdracht/payload/AuthenticationResponse.java
+++ src/main/java/com/example/jubileebackendeindopdracht/payload/AuthenticationResponse.java
@@ -1,4 +1,13 @@
 package com.example.jubileebackendeindopdracht.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+
 public class AuthenticationResponse {
+
+    private final String jwt;
+
 }
