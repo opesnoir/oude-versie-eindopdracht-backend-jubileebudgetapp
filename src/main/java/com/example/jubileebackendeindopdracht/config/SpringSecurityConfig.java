9ba42c48d3b39c6761af507b87b8949798c3c872
opesnoir <goodsurroundsme@outlook.com>
2023-06-20 11:35:48 +0200

    SpringSecurityConfig begonnen, basis code erin van huiswerkles en techiteasy opdracht. daarna antmatchers voor user gemaakt. de rest moet nog

--- src/main/java/com/example/jubileebackendeindopdracht/config/SpringSecurityConfig.java
+++ src/main/java/com/example/jubileebackendeindopdracht/config/SpringSecurityConfig.java
@@ -1,5 +1,86 @@
 package com.example.jubileebackendeindopdracht.config;
 
 //TODO: deze klaar, check dan: AuthenticationController, of melding Could not autowire. No beans of 'AuthenticationManager' type found. weg is in constructor.
+
+import com.example.jubileebackendeindopdracht.filter.JwtRequestFilter;
+import com.example.jubileebackendeindopdracht.services.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+    public SpringSecurityConfig(CustomUserDetailsService customUserDetailsService, JwtRequestFilter jwtRequestFilter) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // authentication
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
+    }
+
+    // authorization with jwt
+    @Bean
+    protected SecurityFilterChain filter (HttpSecurity http) throws Exception{
+
+        http
+                .csrf().disable()
+                .httpBasic().disable()
+                .cors().and()
+                .authorizeHttpRequests()
+
+                // user
+                .antMatchers(HttpMethod.POST, "/users").permitAll()
+                .antMatchers(HttpMethod.GET, "/users").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/users/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.PUT, "/users/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/users/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
+
+                // account
+                // balance
+                // profile
+                // saving goal
+                // transaction
+                // upload
+
+
+                // authentication
+                .antMatchers("/authenticated").authenticated()
+                .antMatchers("/authenticate").permitAll()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+
+    }
+
 }
