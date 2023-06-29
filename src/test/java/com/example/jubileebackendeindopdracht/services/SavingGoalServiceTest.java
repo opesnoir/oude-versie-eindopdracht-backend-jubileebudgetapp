6b186d0f1d298a14a8f7aef6d07750429699c229
opesnoir <goodsurroundsme@outlook.com>
2023-06-29 10:06:42 +0200

    generated test voor transactionservice en savinggoalservice

--- src/test/java/com/example/jubileebackendeindopdracht/services/SavingGoalServiceTest.java
+++ src/test/java/com/example/jubileebackendeindopdracht/services/SavingGoalServiceTest.java
@@ -1,10 +1,20 @@
 package com.example.jubileebackendeindopdracht.services;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SavingGoalServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
 
     @Test
     void getAllSavingGoals() {
